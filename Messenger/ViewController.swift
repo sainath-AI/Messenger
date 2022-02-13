af00dd3f34560fe68ca65a117a9834e967fec4d9
sainath-AI <dukedustine123@gmail.com>
2022-02-14 00:23:57 +0530

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Perennial Systems on 14/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
